8768bf98b0a49f964136ecf2ef0bb9f2073c76cc
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-04-29 00:53:12 +0600

    add phero real file

--- index.js
+++ index.js
@@ -1,112 +1,102 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const ObjectId = require('mongodb').ObjectId;
 const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
 
 const app = express();
-const port = process.env.PORT || 5000
 
 // middleware
-app.use(express.json())
 app.use(cors());
+app.use(express.json());
 
-// verifyJWT  login token function 
 function verifyJWT(req, res, next) {
-    const authHeader = req.headers.authorization
+    const authHeader = req.headers.authorization;
     if (!authHeader) {
         return res.status(401).send({ message: 'unauthorized access' });
     }
     const token = authHeader.split(' ')[1];
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
-            return res.status(403).send({ message: 'Forbidden access' })
+            return res.status(403).send({ message: 'Forbidden access' });
         }
-        console.log('decoded', decoded)
+        console.log('decoded', decoded);
         req.decoded = decoded;
         next();
-
     })
 }
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wlave.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vwx9p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-
 
 async function run() {
     try {
         await client.connect();
-        const servicesCollection = client.db("geniusCar").collection("services");
-        const ordersCollection = client.db("geniusCar").collection("orders");
+        const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection = client.db('geniusCar').collection('order');
 
-        // AUTH //
+        // AUTH
         app.post('/login', async (req, res) => {
-            const user = req.body
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
                 expiresIn: '1d'
             });
-            res.send(token)
+            res.send({ accessToken });
         })
 
-
-        // services section API
-
-        // get all services
+        // SERVICES API
         app.get('/service', async (req, res) => {
-            const query = {}
-            const cursor = servicesCollection.find(query)
+            const query = {};
+            const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
-            res.send(services)
-        })
+            res.send(services);
+        });
 
-        // get service by id
         app.get('/service/:id', async (req, res) => {
-            const id = req.params.id
-            const query = { _id: ObjectId(id) }
-            const service = await servicesCollection.findOne(query)
+            const id = req.params.id;
+            console.log(id);
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
             res.send(service);
-        })
+        });
 
-        //post 
+        // POST
         app.post('/service', async (req, res) => {
-            const newService = req.body
-            const result = await servicesCollection.insertOne(newService)
-            res.send(result)
-        })
+            const newService = req.body;
+            const result = await serviceCollection.insertOne(newService);
+            res.send(result);
+        });
 
-        //delete service 
+        // DELETE
         app.delete('/service/:id', async (req, res) => {
-            const id = req.params.id
-            const query = { _id: ObjectId(id) }
-            const result = await servicesCollection.deleteOne(query)
-            res.send(result)
-        })
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await serviceCollection.deleteOne(query);
+            res.send(result);
+        });
 
-        //  order section API 
+        // Order Collection API
 
-         // get orders 
-         app.get('/order', verifyJWT, async (req, res) => {
-            const decodedEmail = req.decoded.email
-            const email = req.query.email
+        app.get('/order', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
             if (email === decodedEmail) {
-                const query = { email: email }
-                const cursor = ordersCollection.find(query)
-                const result = await cursor.toArray()
-                res.send(result)
+                const query = { email: email };
+                const cursor = orderCollection.find(query);
+                const orders = await cursor.toArray();
+                res.send(orders);
             }
-            else {
-                return res.status(403).send({ message: 'Forbidden access' })
+            else{
+                res.status(403).send({message: 'forbidden access'})
             }
         })
 
-        //post to  add order
         app.post('/order', async (req, res) => {
-            const order = req.body
-            const result = await ordersCollection.insertOne(order)
-            res.send(result)
+            const order = req.body;
+            const result = await orderCollection.insertOne(order);
+            res.send(result);
         })
 
     }
@@ -114,15 +104,17 @@ async function run() {
 
     }
 }
+
 run().catch(console.dir);
 
-
-
-
 app.get('/', (req, res) => {
-    res.send('runnnnnnnnnning')
+    res.send('Running Genius Server');
+});
+
+app.get('/hero', (req, res) =>{
+    res.send('Hero meets hero ku')
 })
 
 app.listen(port, () => {
-    console.log('listening to port:', port);
+    console.log('Listening to port', port);
 })
