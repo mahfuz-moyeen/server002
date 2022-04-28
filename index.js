5bd83d2b8f8a44d3a2dee4f6d4afbe6ae7e7e9c5
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-04-28 23:57:59 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const port = process.env.PORT || 5000
+
+app.use(express.json())
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('runnnnnnnnnning')
+})
+
+app.listen(port, () => {
+    console.log('listening to port:', port);
+})
